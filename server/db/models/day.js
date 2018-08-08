fe0e69f2fda117bde339a42cce45a11e30286e55
the97bradley <the97bradley@gmail.com>
2018-08-08 11:10:23 -0500

    models work

---
+++ server/db/models/day.js
@@ -0,0 +1,32 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+const Meal = require('./meal')
+
+const Day = db.define('day', {
+
+  calories: {
+    type: Sequelize.INTEGER
+  },
+  description: {
+    type: Sequelize.TEXT
+  }
+
+})
+
+
+const setMeals = async day => {
+  const mealArr = await Promise.all([Meal.create({type: 'breakfast'}), Meal.create({type: 'lunch'}),
+    Meal.create({type: 'dinner'}), Meal.create({type: 'snacks'})])
+
+  await day.setMeals(mealArr)
+
+
+}
+
+Day.afterCreate(setMeals)
+
+
+module.exports = Day
+
+
+
